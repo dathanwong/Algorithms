e88080a9f5728bdbd5ae570e627563599c77b403
dathwo@outlook.com <dathwo@outlook.com>
2020-07-09 10:21:00 -0700

    Added a partitioning function for quick sort

--- SortingAlgos.js
+++ SortingAlgos.js
@@ -192,3 +192,48 @@ function combineSortedArrays(arr1, arr2) {
   
   console.log(mergeSort([1, 5, 2, 8, 3, 4]));
   // should log [1, 2, 3, 4, 5, 8]
+
+  /**
+ * takes in an array, a start index, and an end index
+ * picks a random pivot index around which values will be partitioned
+ * returns an integer: the new "pivot" index
+ * the array should be mutated by the function
+ */
+
+function arrayPartition(arr, startIdx = 0, endIdx = arr.length - 1) {
+    let pivot = Math.floor(Math.random()*(endIndex - startIndex + 1) );
+    let pivotValue = arr[pivot];
+    while (startIdx != endIdx) {
+      while(arr[startIdx] < pivotValue) {
+        startIdx++;
+      }
+      while(arr[endIdx] > pivotValue ) {
+        endIdx--;
+      }
+      // if(pivot == endIdx) {
+      //   pivot = startIdx;
+      // }
+      // else if(pivot == startIdx) {
+      //   pivot = endIdx;
+      }
+      let temp = arr[startIdx];
+      arr[startIdx] = arr[endIdx];
+      arr[endIdx] = temp;
+      return startIdx; // or end index
+  }
+    
+    
+    // Math.random() returns a number from 0 up to but excluding 1
+    // Math.floor(5.614) returns 5
+    
+    const arr = [1, 2, 5, 4, 8, 6]
+    console.log(arrayPartition(arr)); // should log an index between 0 and 5
+    console.log(arr); // should be mutated and partitioned around the returned index
+    
+    // all values should be partitioned around the returned pivot index
+    // for example, if your function chooses a pivot of 3,
+    // the returned pivot should be 2 (the new pivot index)
+    // the array might now be [1, 2, 4, 8, 5, 6]
+    // 4 has all smaller values to its left and all greater values to its right
+  //   *************************************************************************************************
+  
